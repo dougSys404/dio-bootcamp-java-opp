7323e667b7066ad542799dd310b35ed90a5632cf
Doug <douglas@macOs.local>
2025-05-10 23:29:03 -0300

    Implementing methods of dev class

--- src/br/com/dougSys404/desafio/domain/Dev.java
+++ src/br/com/dougSys404/desafio/domain/Dev.java
@@ -2,6 +2,7 @@ package br.com.dougSys404.desafio.domain;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -14,15 +15,26 @@ public class Dev {
 
 
     public void inscreverBootCamp(Bootcamp bootcamp){
-
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        // adiciona o dev no bootcamp, o this passa o objeto dev inteiro.
+        bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir(){
-
+        Optional<Conteudo> conteudo =  this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
     }
 
-    public void calcularTotalXp(){
-
+    public double calcularTotalXp(){
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(conteudo -> conteudo.calcularXp())
+                .sum();
     }
 
     public Set<Conteudo> getConteudosConcluidos() {
